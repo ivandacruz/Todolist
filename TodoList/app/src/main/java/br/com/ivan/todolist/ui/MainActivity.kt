0ac1117302c6a5e96b2cec5dfe94fadd0f17c806
ivandacruz <64236894+ivandacruz@users.noreply.github.com>
2021-10-01 09:34:38 -0300

    First commit

---
+++ TodoList/app/src/main/java/br/com/ivan/todolist/ui/MainActivity.kt
@@ -0,0 +1,58 @@
+package br.com.ivan.todolist.ui
+
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.ivan.todolist.databinding.ActivityMainBinding
+import com.ivan.todolist.datasource.TaskDataSource
+import com.ivan.todolist.ui.TaskListAdapter
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val adapter by lazy { TaskListAdapter() }
+    private val register =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == Activity.RESULT_OK) updateList()
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.rvTasks.adapter = adapter
+        updateList()
+
+        insertListeners()
+    }
+
+    private fun insertListeners() {
+        binding.root.setOnClickListener {
+            register.launch(Intent(this, AddTaskActivity::class.java))
+        }
+
+        adapter.listenerEdit = {
+            val intent = Intent(this, AddTaskActivity::class.java)
+            intent.putExtra(AddTaskActivity.TASK_ID, it.id)
+            register.launch(intent)
+        }
+
+        adapter.listenerDelete = {
+            TaskDataSource.deleteTask(it)
+            updateList()
+        }
+    }
+
+    private fun updateList() {
+        val list = TaskDataSource.getList()
+        binding.includeEmpty.emptyState.visibility = if (list.isEmpty()) View.VISIBLE
+        else View.GONE
+
+        adapter.submitList(list)
+    }
+}
